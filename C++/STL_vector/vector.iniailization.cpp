79bdaf26c3c821f87062450b9761d881c52d0c23
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2025-01-03 09:46:59 +0600

    vector inialization

---
+++ C++/STL_vector/vector.iniailization.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    // vector<int>v;
+    // vector<int>v(5);
+      vector<int>v(5,10);
+
+    for(int i =0; i<v.size(); i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    cout<<v.size()<<endl;
+    return 0;
+
+}
